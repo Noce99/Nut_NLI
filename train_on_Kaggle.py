d24924f20d6769cd30dfec1d9a6dd960b8d415ba
Enrico Mannocci <enrico.mannocci99@gmail.com>
2023-04-22 16:50:19 +0200

    final commit

--- train_on_Kaggle.py
+++ train_on_Kaggle.py
@@ -2,11 +2,8 @@ import time
 import torch
 from torch import nn
 from tqdm import tqdm
-<<<<<<< HEAD
-from transformers import XLMRobertaTokenizer, XLMRobertaModel, get_linear_schedule_with_warmup, RobertaForSequenceClassification
-=======
-from transformers import XLMRobertaTokenizer, XLMRobertaModel, get_linear_schedule_with_warmup
->>>>>>> d4419b454f88f125186481cabf194c77394f36c3
+from transformers import XLMRobertaModel, get_linear_schedule_with_warmup, RobertaForSequenceClassification, RobertaTokenizer
+from transformers import XLMRobertaTokenizer, XLMRobertaForSequenceClassification
 from torch.optim import AdamW
 from prettytable import PrettyTable
 import pandas as pd
@@ -25,6 +22,8 @@ class NutNLIModel(nn.Module):
         self.training_labels = []
         self.validation_pairs = []
         self.validation_labels = []
+        self.test_ids = []
+        self.test_pairs = []
 
         # set up training stuff
         self.loss_fn = nn.CrossEntropyLoss()
@@ -32,14 +31,8 @@ class NutNLIModel(nn.Module):
         self.history_loss = []
         self.scheduler = None
 
-        # get the Bert Model
-<<<<<<< HEAD
-        self.roberta = XLMRobertaModel.from_pretrained("symanto/xlm-roberta-base-snli-mnli-anli-xnli")
-        # self.roberta = RobertaForSequenceClassification.from_pretrained("roberta-large", num_labels=3)
-        # self.roberta.load_state_dict(torch.load("./NutModel/model.pt"))
-=======
-        self.roberta = XLMRobertaModel.from_pretrained("xlm-roberta-base")  # "symanto/xlm-roberta-base-snli-mnli-anli-xnli")
->>>>>>> d4419b454f88f125186481cabf194c77394f36c3
+        self.roberta = XLMRobertaForSequenceClassification.from_pretrained("xlm-roberta-large", num_labels=3)
+        self.roberta.load_state_dict(torch.load("./MyModel/model.pt"))
         self.tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
 
         # get some information from the bert model
@@ -56,10 +49,9 @@ class NutNLIModel(nn.Module):
         for p_name, p in self.roberta.named_parameters():
             # print(f"'{p_name}' -> {p.shape}")
             splitted_name = p_name.split('.')
-            if splitted_name[0] == 'pooler' or splitted_name[0] == 'encoder' and \
-                    int(splitted_name[2]) >= self.num_bert_layer - self.num_of_layers_to_unfreeze:
+            if splitted_name[0] == 'classifier' or (len(splitted_name) >= 4 and splitted_name[3] == '23'):
                 print(f"Unfreezing [{p_name} ({p.shape})]")
-                # p.data = torch.randn(p.shape) * 0.02  # Random weight initialization
+                p.data = torch.randn(p.shape) * 0.02  # Random weight initialization
                 p.requires_grad = True  # Not Freeze
             else:
                 p.requires_grad = False  # Freeze
@@ -75,12 +67,8 @@ class NutNLIModel(nn.Module):
         """
         # indexed_sentences, sentences_separators, attention_masks = self.from_text_to_bert_input(sentence_pairs)
         input_ids, attention_mask = self.from_text_to_bert_input(sentence_pairs)
-        roberta_pooler_output = self.roberta(input_ids, attention_mask)[1]  # 1 because we only care about the pooler
-        #                                                                     output
-        linear_output_1 = self.linear_nli_1(roberta_pooler_output)
-        linear_output = self.linear_nli_2(linear_output_1)
-        probability = self.softmax(linear_output)
-
+        roberta_output = self.roberta(input_ids, attention_mask).logits
+        probability = self.softmax(roberta_output)
         return probability
 
     def evaluation(self, validation_set=True):
@@ -114,7 +102,29 @@ class NutNLIModel(nn.Module):
         correct_classification = torch.sum(tensor_true == tensor_prediction)
         accuracy = correct_classification / len(labels)
         return float(accuracy)
+    
+    def create_test_labels(self):
+        pairs = self.test_pairs
 
+        evaluation_tqdm = tqdm(range(0, len(pairs), self.batch_size), unit=" Sentences Pairs", desc="Evaluation")
+
+        self.eval()
+
+        all_predicted_labels = []
+
+        for start_batch_index in evaluation_tqdm:
+            predicted_probability = self.forward(pairs[start_batch_index:start_batch_index + self.batch_size])
+            predicted_labels = torch.argmax(predicted_probability, 1)
+            all_predicted_labels += predicted_labels.tolist()
+
+        tensor_prediction = torch.tensor(all_predicted_labels)
+        submission = open('submission.csv', 'w')
+        submission.write(f"id,prediction\n")
+        for i in range(len(self.test_ids)):
+            submission.write(f"{self.test_ids[i]},{int(tensor_prediction[i])}\n")
+        submission.close()
+        
+    
     def from_text_to_bert_input(self, sentence_pairs):
         """
         :param sentence_pairs: A list of tuple containing the sentences as text
@@ -145,7 +155,21 @@ class NutNLIModel(nn.Module):
                 self.validation_labels.append(labels[i])
 
         print(f"Loaded {len(self.training_pairs)} training data and {len(self.validation_pairs)} validation data.")
+    
+    def load_test_dataset(self):
+        df = pd.read_csv("../Data/test.csv")
+        ids = df["id"].values
+        premises = df["premise"].values
+        hypothesis = df["hypothesis"].values
 
+        loading_database_tqdm = tqdm(range(len(premises)), unit=" Sentences Pairs", desc="Loading Dataset")
+
+        for i in loading_database_tqdm:
+            self.test_pairs.append((premises[i], hypothesis[i]))
+            self.test_ids.append(ids[i])
+
+        print(f"Loaded {len(self.test_pairs)} test data")
+    
     def train_step(self, sentence_pairs, true_probability):
         predicted_probability = self.forward(sentence_pairs)
 
@@ -196,7 +220,7 @@ class NutNLIModel(nn.Module):
 
             plt.plot(list(range(len(self.history_loss))), self.history_loss)
             plt.savefig('loss.png')
-
+    
     @staticmethod
     def label_to_probability(labels):
         probabilities = torch.zeros(len(labels), 3, dtype=torch.float)
@@ -207,7 +231,9 @@ class NutNLIModel(nn.Module):
 
 if __name__ == '__main__':
     model = NutNLIModel(batch_size=32, num_of_layers_to_unfreeze=12)
-    # print(model)
-    # model.forward([("I'm a man", "I will not die"), ("I like red", "I love red curtains"), ("A", "We!")])
     model.load_training_dataset()
-    model.train_me(50)
+    model.train_me(3)
+    model.load_test_dataset()
+    model.create_test_labels()
+    
+    #symanto/xlm-roberta-base-snli-mnli-anli-xnli
